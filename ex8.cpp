041a5c30a553cd128a7095450283d300ceee8982
chanhye03 <chanhye0619@gmail.com>
2024-11-22 15:27:50 +0900

    Create ex8.cpp

---
+++ ex8.cpp
@@ -0,0 +1,21 @@
+// 저장 유형 지정자 static
+#include <stdio.h>
+
+void sub() {
+
+	static int scount = 0;
+	int acount = 0;
+	printf("scount = %d\t", scount);
+
+	printf("acount = %d\n", acount);
+	scount++;
+	acount++;
+
+}
+
+int main(void) {
+	sub();
+	sub();
+	sub();
+	return 0;
+}
